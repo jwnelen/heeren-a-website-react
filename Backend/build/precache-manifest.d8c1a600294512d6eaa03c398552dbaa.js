2e39ff543b5ee1158e9f16629082bae59eb95996
jwnelen <jwnelen@gmail.com>
2020-10-15 14:53:37 +0200

    deleted the gitignore for the backend Build folder 2

---
+++ Backend/build/precache-manifest.d8c1a600294512d6eaa03c398552dbaa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d0388b31310af80c047ebeb8177db3e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "09b5a40d7b3de4c301dd",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "699fbce943ecac98e754",
+    "url": "/static/css/main.c4c59b1e.chunk.css"
+  },
+  {
+    "revision": "09b5a40d7b3de4c301dd",
+    "url": "/static/js/2.7d7fcbe5.chunk.js"
+  },
+  {
+    "revision": "542c91f071fe828f7759f188e3ae7231",
+    "url": "/static/js/2.7d7fcbe5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "699fbce943ecac98e754",
+    "url": "/static/js/main.0af58aee.chunk.js"
+  },
+  {
+    "revision": "2773b85b485d8f92a4bb",
+    "url": "/static/js/runtime-main.068009ed.js"
+  }
+]);
